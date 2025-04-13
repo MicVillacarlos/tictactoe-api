24ff80cfa22fc9095af6b4e1c3aad068b795b726
MicVillacarlos <micvillacarlos@gmail.com>
2025-04-13 17:47:29 +0800

    add patch in cors

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 app.use(
   cors({
     origin: config.feUrl,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   })
 );
 
